44f056b6c639c3b5f28be6f71e5068653ac27fa9
Keybinder <nichtmeg@outlook.com>
2025-12-11 01:26:31 +0100

    Add WebSocket support and license check
    Implement WebSocket handling with license validation.

--- worker/app.js
+++ worker/app.js
@@ -1,5 +1,31 @@
+// importer bloggdata
+import content from "./content.json";
 export default {
-  async fetch(request) {
-    return new Response("Hello World from Cloudflare Worker!");
-  },
+  async fetch(request, env, ctx) {
+    // bare behandle websocket oppgradering
+    if (request.headers.get("Upgrade") !== "websocket") {
+      return new Response("Expected WebSocket", { status: 426 });
+    }
+
+    // etabler websocket
+    const pair = new WebSocketPair();
+    const [client, server] = Object.values(pair);
+    
+    // aksepter tilkobling
+    server.accept();
+    
+    // behandle all inndata
+    server.addEventListener("message", (event) => {
+      let response = { status: 403, message: "License not valid" };
+
+      // sjekk om lisensen er gyldig
+      if (env.LICENSES.split(",").includes(event.data)) {
+        response = { status: 200, data: content };
+      }
+
+      // send respons
+      server.send(JSON.stringify(response));
+    });
+    return new Response(null, { status: 101, webSocket: client });
+  }
 };
